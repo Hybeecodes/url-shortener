70c7f8251b80d3e6a37a9f8dea2fc557d7aa5476
megastar98 <obikoya11@gmail.com>
2018-03-19 17:21:01 +0100

    created db model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const base64 = require('base64-js');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/url_shortner',function(req,res){
   const long__url = req.body.long_url;
-  
+
 })
 
 module.exports = router;
