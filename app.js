5c2639f43d373c7766b122e6678a8dcf66c80764
megastar98 <obikoya11@gmail.com>
2018-03-21 06:50:42 +0100

    few changes

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./db_config');
+const cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +21,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- models/short_urls.js
+++ models/short_urls.js
@@ -1,4 +1,4 @@
-import { mongo } from 'mongoose';
+
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
